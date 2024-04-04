222bbb483b2e533c7f9bad009d6ac4eb81cb9734
Natalia G Dorado <148120776+nataliagdorado@users.noreply.github.com>
2024-04-04 20:19:57 +0000

    +

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -4,15 +4,23 @@ import java.io.InputStreamReader;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bffRead = new BufferedReader(new InputStreamReader(System.in));
         try {
-            System.out.print("Enter a number: ");
-            String input = reader.readLine();
-            int number = Integer.parseInt(input);
-            System.out.println("You entered: " + number);
+            System.out.print("Introduce un número para calcular los n números naturales que hay antes que él:");
+            String input = bffRead.readLine();
+            int numero = Integer.parseInt(input);
+            int suma=suma(numero);
+            System.out.println("La suma de los " + numero + " naturales es" + suma);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    public static int suma(int n) {
+        if ( n==0){
+            return 0;
+        } else {
+            return n + suma(n-1);
+            
+    }
+}
 }
---
+++ Ejercicio2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Ejercicio2 {
+    public static void maint(String[] args) {
+        BufferedReader bffReader = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
